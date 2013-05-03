31b57c91a446fbec21cae758c744f2d0d8eb20fc
Raphael Fabeni <rapha.fabeni@gmail.com>
2013-05-03 13:53:54 -0300

    Comentario scripts / ajustes links

--- index.php
+++ index.php
@@ -15,21 +15,21 @@
       <div class="row projects">
          <section class="project">
             <h2>CD Framework</h2>
-            <a href="http://lab.a2comunicacao.com.br/cdframework/" class="btn"><i class="icon-eye-open"></i>Demo</a>
+            <a target="_blank" href="http://lab.a2comunicacao.com.br/cdframework/" class="btn"><i class="icon-laptop"></i>Demo</a>
             <a href="https://github.com/a2comunicacao/cdframework/archive/master.zip" class="btn"><i class="icon-download"></i>Download</a>
-            <a href="https://github.com/a2comunicacao/cdframework" class="btn"><i class="icon-github"></i>Github</a>
+            <a target="_blank" href="https://github.com/a2comunicacao/cdframework" class="btn"><i class="icon-github"></i>Github</a>
          </section>
          <section class="project">
             <h2>WP Plugins</h2>
-            <a href="http://lab.a2comunicacao.com.br/wp-plugins/" class="btn"><span><i class="icon-eye-open"></i>Demo</span></a>
+            <a target="_blank" href="http://lab.a2comunicacao.com.br/wp-plugins/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
             <a href="https://github.com/a2comunicacao/wp-plugins/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a href="https://github.com/a2comunicacao/wp-plugins" class="btn"><span><i class="icon-github"></i>Github</span></a>
+            <a target="_blank" href="https://github.com/a2comunicacao/wp-plugins" class="btn"><span><i class="icon-github"></i>Github</span></a>
          </section>
          <section class="project">
             <h2>jQuery Editable</h2>
-            <a href="http://lab.a2comunicacao.com.br/jquery-editable/" class="btn"><span><i class="icon-eye-open"></i>Demo</span></a>
+            <a target="_blank" href="http://lab.a2comunicacao.com.br/jquery-editable/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
             <a href="https://github.com/williancarminato/jquery-editable/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a href="https://github.com/williancarminato/jquery-editable" class="btn"><span><i class="icon-github"></i>Github</span></a>
+            <a target="_blank" href="https://github.com/williancarminato/jquery-editable" class="btn"><span><i class="icon-github"></i>Github</span></a>
          </section>
 
       </div>
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,2 @@
 jQuery(document).ready(function($) {
-   $('.btn').on('click', function(event){
-      event.preventDefault();
-      console.log('ae');
-   });
 });
