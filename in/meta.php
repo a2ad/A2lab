f2aaed4fe077487f67e2b6a2b88f568670d623a4
Raphael Fabeni <rapha.fabeni@gmail.com>
2013-05-28 13:49:00 -0300

    Correcoes no arquivo meta

--- in/meta.php
+++ in/meta.php
@@ -6,11 +6,12 @@
 <!-- Facebook -->
 <meta property="fb:app_id" content="520168484686319" />
 <meta property="fb:admins" content="100003036613503" />
+<meta property="article:author" content="http://www.a2comunicacao.com.br" />
 <meta property="og:site_name" content="A2Lab"/>
 <meta property="og:title" content="A2Lab | Projeto interno da equipe de desenvolvimento da A2Comunicação" />
 <meta property="og:type" content="article" />
 <meta property="og:url" content="http://lab.a2comunicacao.com.br" />
-<meta property="og:image" content="http://lab.a2comunicacao.com.br/img/logo-meta.jpg" />
+<meta property="og:image" content="http://lab.a2comunicacao.com.br/img/logo-meta.png" />
 
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="author" href="humans.txt" />
