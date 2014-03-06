a8377ed3686f831b0e31b079f8500ae30c6d7406
Vivian Oliveira <vivinhaa@gmail.com>
2014-03-06 14:35:36 -0300

    Inclusão de projetos e organização em ordem alfabética

--- index.php
+++ index.php
@@ -13,64 +13,115 @@
    <?php include('in/header.php');?>   
    <div role="main" class="container main">
       <div class="row">
+
          <section class="project project-01">
+            <h2>A2boilerplate</h2>
+            <p>Boilerplate para dar o 'pontapé inicial' em um projeto web</p>
+            <a href="https://github.com/a2comunicacao/A2boilerplate/archive/master.zip" class="btn"><i class="icon-download"></i>Download</a>
+            <a target="_blankl" href="https://github.com/a2comunicacao/A2boilerplate" class="btn"><i class="icon-github"></i>Github</a>
+         </section>
+
+         <section class="project project-02">
+            <h2>A2showcase</h2>
+            <p>Catálogo  de sites de governo</p>
+            <a target="_blank" href="https://gimmebar.com/loves/a2showcase" class="btn"><i class="icon-laptop"></i>Demo</a>
+         </section>
+
+         <section class="project last project-03">
             <h2>CD Framework</h2>
+            <p>Framework para montagem de CD-ROMs</p>
             <a target="_blank" href="http://lab.a2comunicacao.com.br/cdframework/" class="btn"><i class="icon-laptop"></i>Demo</a>
             <a href="https://github.com/a2comunicacao/cdframework/archive/master.zip" class="btn"><i class="icon-download"></i>Download</a>
             <a target="_blank" href="https://github.com/a2comunicacao/cdframework" class="btn"><i class="icon-github"></i>Github</a>
          </section>
-         <section class="project project-02">
-            <h2>WP Plugins</h2>
-            <a target="_blank" href="http://lab.a2comunicacao.com.br/wp-plugins/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
-            <a href="https://github.com/a2comunicacao/wp-plugins/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a target="_blank" href="https://github.com/a2comunicacao/wp-plugins" class="btn"><span><i class="icon-github"></i>Github</span></a>
-         </section>
-         <section class="project last project-03">
-            <h2>jQuery Editable</h2>
-            <a target="_blank" href="http://lab.a2comunicacao.com.br/jquery-editable/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
-            <a href="https://github.com/williancarminato/jquery-editable/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a target="_blank" href="https://github.com/williancarminato/jquery-editable" class="btn"><span><i class="icon-github"></i>Github</span></a>
-         </section>
       </div>
+
       <div class="row">
+
          <section class="project project-04">
-            <h2>Font-Size</h2>
-            <a target="_blank" href="http://airton.github.io/Font-Size/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
-            <a href="https://github.com/airton/Font-Size/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a target="_blank" href="https://github.com/airton/Font-Size" class="btn"><span><i class="icon-github"></i>Github</span></a>
-         </section>
-         <section class="project project-05">
-            <h2>Vídeo Destacado</h2>
-            <a target="_blank" href="http://wordpress.org/plugins/video-destacado/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
-            <a href="https://github.com/airton/video-destacado/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a target="_blank" href="https://github.com/airton/video-destacado" class="btn"><span><i class="icon-github"></i>Github</span></a>
-         </section>
-         <section class="project last project-06">
-            <h2>jQuery Accordion</h2>
-            <a target="_blank" href="http://raphaelfabeni.github.io/accordion/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
-            <a href="https://github.com/raphaelfabeni/accordion/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
-            <a target="_blank" href="https://github.com/raphaelfabeni/accordion" class="btn"><span><i class="icon-github"></i>Github</span></a>
-         </section>
-      </div>
-      <div class="row">
-         <section class="project project-07">
             <h2>CSS3 Transition Snippets</h2>
+            <p>Laboratório sobre a propriedade Transition do CSS3</p>
             <a target="_blank" href="http://raphaelfabeni.github.io/transitionSnippets" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
             <a href="https://github.com/raphaelfabeni/transitionSnippets/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
             <a target="_blank" href="https://github.com/raphaelfabeni/transitionSnippets" class="btn"><span><i class="icon-github"></i>Github</span></a>            
          </section>
+
+         <section class="project project-05">
+            <h2>Font-Size</h2>
+            <p>Plugin que facilita a criação dos botões A+ e A-</p>
+            <a target="_blank" href="http://airton.github.io/font-size/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
+            <a href="https://github.com/airton/Font-Size/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
+            <a target="_blank" href="https://github.com/airton/Font-Size" class="btn"><span><i class="icon-github"></i>Github</span></a>
+         </section>
+
+         <section class="project last project-06">
+            <h2>jQuery Accordion</h2>
+            <p>Plugin para mostrar conteúdo com o efeito accordion</p>
+            <a target="_blank" href="http://raphaelfabeni.github.io/accordion/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
+            <a href="https://github.com/raphaelfabeni/accordion/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
+            <a target="_blank" href="https://github.com/raphaelfabeni/accordion" class="btn"><span><i class="icon-github"></i>Github</span></a>
+         </section>
+
+      </div>
+
+      <div class="row">
+
+         <section class="project project-07">
+            <h2>jQuery Editable</h2>
+            <p>Plugin para fazer blocos de página editáveis</p>
+            <a target="_blank" href="http://lab.a2comunicacao.com.br/jquery-editable/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
+            <a href="https://github.com/williancarminato/jquery-editable/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
+            <a target="_blank" href="https://github.com/williancarminato/jquery-editable" class="btn"><span><i class="icon-github"></i>Github</span></a>
+         </section>
+
          <section class="project project-08">
+            <h2>Metodologia A2</h2>
+            <p>Documentação de todos os processos da A2</p>
+            <a target="_blank" href="https://github.com/a2comunicacao/metodologia" class="btn"><span><i class="icon-github"></i>Github</span></a>
+         </section>
+
+         <section class="project last project-09">
             <h2>Twitter API</h2>
+            <p>Plugin para mostrar os últimos tweets de uma conta</p>
             <a target="_blank" href="http://lab.a2comunicacao.com.br/twitter-api/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
             <a href="http://lab.a2comunicacao.com.br/twitter-api/twitterfeed.min.js" class="btn"><span><i class="icon-download"></i>Download</span></a>
             <a target="_blank" href="https://github.com/airton/twitter-api" class="btn"><span><i class="icon-github"></i>Github</span></a>
          </section>
-         <section class="project last project-09">
+
+      </div>
+
+      <div class="row">
+
+         <section class="project project-10">
+            <h2>Vídeo Destacado</h2>
+            <p>Plugin Wordpress para definir vídeo como destaque </p>
+            <a target="_blank" href="http://wordpress.org/plugins/video-destacado/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
+            <a href="https://github.com/airton/video-destacado/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
+            <a target="_blank" href="https://github.com/airton/video-destacado" class="btn"><span><i class="icon-github"></i>Github</span></a>
+         </section>
+
+         <section class="project project-11">
+            <h2>WP Plugins</h2>
+            <p>Plugins de Wordpress mais utilizados pela equipe da A2</p>
+            <a target="_blank" href="http://lab.a2comunicacao.com.br/wp-plugins/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
+            <a href="https://github.com/a2comunicacao/wp-plugins/archive/master.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
+            <a target="_blank" href="https://github.com/a2comunicacao/wp-plugins" class="btn"><span><i class="icon-github"></i>Github</span></a>
+         </section>
+
+         <section class="project last project-12">
             <h2>WP samples</h2>
+            <p>Coletânea de snippets para projetos em Wordpress</p>
             <a target="_blank" href="http://raphaelfabeni.github.io/wp-samples/" class="btn"><span><i class="icon-laptop"></i>Demo</span></a>
             <a href="https://github.com/raphaelfabeni/wp-samples/archive/gh-pages.zip" class="btn"><span><i class="icon-download"></i>Download</span></a>
             <a target="_blank" href="https://github.com/raphaelfabeni/wp-samples/" class="btn"><span><i class="icon-github"></i>Github</span></a>
          </section>
+         
+      </div>
+
+
+
+
+
       </div>
    </div>
 
